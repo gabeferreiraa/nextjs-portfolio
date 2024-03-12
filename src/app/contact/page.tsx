95efcb7bcce15c1fdfe1fceed205860183cbeccf
gabeferreiraa <146259686+gabeferreiraa@users.noreply.github.com>
2024-03-12 16:19:10 -0400

    updated deps

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ContactPage = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <section className="w-full h-[75vh] bg-black">
+        Content
+        <div>help</div>
+      </section>
+      <section className="w-full h-[70vh] bg-navy">More Content</section>
+    </>
+  );
 };
 
 export default ContactPage;
