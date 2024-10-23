191573a94675eefd0bac97523b95ec9784c9347a
gabeferreiraa <146259686+gabeferreiraa@users.noreply.github.com>
2024-10-22 23:10:43 -0400

    sizing changes, ready to add new images 3

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -29,37 +29,37 @@ const ContactPage = () => {
           <br />
           RRR1ngg...
         </div>
-        <div className="text-white w-96 h-fit sm:w-fit font-azeret_mono text-3xl tracking-[.6rem] absolute bottom-1/2 right-10 hover:text-yellow">
+        <div className="text-white w-96 h-fit sm:w-fit font-azeret_mono text-3xl tracking-[.6rem] absolute bottom-1/3 right-10 hover:text-yellow">
           +ry!ng t0 r3@ch m3 ~/?
         </div>
-        <div className="text-white w-fit h-fit font-azeret_mono text-3xl tracking-[.6rem] absolute inset-y-2/3 sm:inset-y-3/4 inset-x-24 hover:text-yellow">
+        <div className="text-white w-fit h-fit font-azeret_mono text-3xl tracking-[.6rem] absolute bottom-1/4 left-10 sm:left-24 hover:text-yellow">
           MŸ ll1nk$:{" "}
         </div>
       </section>
-      <section className="w-full h-[80vh] flex flex-col justify-center items-center gap-24 sm:relative bg-black text-white font-azeret_mono text-sm sm:text-lg tracking-[.3rem]">
-        <div className="bottom-20 left-36 sm:absolute h-fit w-fit ">
-          <Link className=" " href="mailto:gferreira0404@gmail.com">
+      <section className="w-full h-[80vh] flex flex-col justify-center items-center gap-10 sm:relative bg-black text-white font-azeret_mono text-sm sm:text-lg tracking-[.3rem]">
+        <div className="sm:absolute sm:bottom-20 sm:left-24 flex flex-col items-center space-y-2">
+          <Link href="mailto:gferreira0404@gmail.com">
             <FolderIcon color={"white"} />
           </Link>
-          Email
+          <span>Email</span>
         </div>
-        <div className="top-40 right-40 sm:absolute h-fit w-fit">
+        <div className="sm:absolute sm:top-20 sm:right-24 flex flex-col items-center space-y-2">
           <Link href="https://github.com/gabeferreiraa">
             <FolderIcon color={"white"} />
           </Link>
-          GitHub
+          <span>GitHub</span>
         </div>
-        <div className="inset-y-1/2 inset-x-1/2 sm:absolute h-fit w-fit">
+        <div className="sm:absolute sm:inset-y-1/2 sm:inset-x-1/2 flex flex-col items-center space-y-2">
           <Link href="https://www.linkedin.com/in/gabriel-ferreira-544b95251/">
             <FolderIcon color={"white"} />
           </Link>
-          LinkedIn
+          <span>LinkedIn</span>
         </div>
-        <div className="inset-y-1/4 inset-x-96 sm:absolute h-fit w-fit ">
+        <div className="sm:absolute sm:inset-y-1/4 sm:left-[35%] flex flex-col items-center space-y-2">
           <Link href="tel:6093517981">
             <FolderIcon color={"white"} />
           </Link>
-          Phone
+          <span>Phone</span>
         </div>
       </section>
     </>
